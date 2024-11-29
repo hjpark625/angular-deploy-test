921b91e3a9672c49a2278d8860ca061d3d4b767b
jeff <jeff@moduparking.com>
2024-11-29 19:23:03 +0900

    refactor: set-env edit

--- set-env.js
+++ set-env.js
@@ -1,24 +1,19 @@
-const { writeFileSync } = require('fs')
-const { resolve } = require('path')
+const { writeFileSync, mkdirSync, existsSync } = require('fs')
+const { resolve, join } = require('path')
 const dotenv = require('dotenv')
 
 const envFile = './.env'
 const fullEnvPath = resolve(__dirname, envFile)
-
 dotenv.config({ path: fullEnvPath, encoding: 'utf8', debug: true })
 
-const prodEnv = './src/environments/environment.ts'
-const devEnv = './src/environments/environment.development.ts'
+const basePath = resolve(__dirname, 'src/environments')
 
-const prodEnvPath = resolve(__dirname, prodEnv)
-const devEnvPath = resolve(__dirname, devEnv)
+if (!existsSync(basePath)) {
+  mkdirSync(basePath, { recursive: true })
+}
 
-// const envPath =
-//   process.env['NODE_ENV'] === 'production'
-//     ? './src/environments/environment.ts'
-//     : './src/environments/environment.development.ts'
-
-// const targetPath = resolve(__dirname, envPath)
+const prodEnvPath = join(basePath, 'environment.ts')
+const devEnvPath = join(basePath, 'environment.development.ts')
 
 const envConfigFile = `export const environment = {
   production: '${process.env['NODE_ENV'] === 'production'}',
@@ -26,9 +21,7 @@ const envConfigFile = `export const environment = {
 }
 `
 
-// writeFileSync(targetPath, envConfigFile)
 writeFileSync(prodEnvPath, envConfigFile)
-
 writeFileSync(devEnvPath, envConfigFile)
 
 console.log(`<=======env start=======>`)
