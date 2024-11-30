314fde58a2ceb830c6957ee1c67228623644afcb
hjpark625 <zero950@gmail.com>
2024-11-30 16:19:25 +0900

    refactor: hashrotuer

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,10 @@
 import { provideZoneChangeDetection } from '@angular/core'
-import { provideRouter } from '@angular/router'
+import { withHashLocation, provideRouter } from '@angular/router'
 
 import { routes } from './app.routes'
 
 import type { ApplicationConfig } from '@angular/core'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withHashLocation())]
 }
